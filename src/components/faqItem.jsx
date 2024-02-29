94809555d57f12231a38a01b1f529cf8e5bcf10b
arianrdcas <arianrdcas@gmail.com>
2024-02-29 16:10:49 -0300

    terminado

---
+++ src/components/faqItem.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "../App.css";
+
+import iconPlus from "../assets/icon-plus.svg";
+import iconMinus from "../assets/icon-minus.svg";
+
+
+
+const FAQItem = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <section>
+        <p>{question}</p>
+        <img
+          className="iconplus"
+          src={isOpen ? iconMinus : iconPlus}
+          alt="Botón"
+          onClick={toggleAccordion}
+        />
+      </section>
+      <div className="articulo">
+        {isOpen && ( 
+            <article>{answer}</article>
+        )}
+      </div>
+    </>
+  );
+}
+
+
+export default FAQItem;
